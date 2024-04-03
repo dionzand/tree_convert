94d1418be8b7553d5d5498d2136eef7346f1a7ac
dionzand <dion.zandstra@gmail.com>
2024-04-03 11:06:40 +0200

    Missing SNPs

--- main.py
+++ main.py
@@ -32,91 +32,107 @@ def get_identifying_snps_for_path(path, hg_to_snp_dict):
     return snps
 
 
-isogg = st.text_input("Enter ISOGG haplogroup")
-yfull = st.text_input("Enter YFull haplogroup")
+col1, col2 = st.columns(2)
 
-if "*" in isogg:
-    isogg = isogg.split("*")[0]
-if "*" in yfull:
-    yfull = yfull.split("*")[0]
+with col1:
+    isogg = st.text_input("Enter ISOGG haplogroup")
+with col2:
+    yfull = st.text_input("Enter YFull haplogroup")
 
-isogg_path = get_path_to_root(isogg_tree, isogg)
-yfull_path = get_path_to_root(yfull_tree, yfull)
+run_button = st.button("Search")
 
-isogg_identifying_snps = [i for i in isogg_hg_to_snp_dict.get(isogg)]  # if i in yfull_snp_to_hg_dict
-yfull_identifying_snps = [i for i in yfull_hg_to_snp_dict.get(yfull)]  # if i in isogg_snp_to_hg_dict
+if run_button:
+    if "*" in isogg:
+        isogg = isogg.split("*")[0]
+    if "*" in yfull:
+        yfull = yfull.split("*")[0]
 
-isogg_missing_snps = [i for i in isogg_identifying_snps if i not in yfull_snp_to_hg_dict]
-yfull_missing_snps = [i for i in yfull_identifying_snps if i not in isogg_snp_to_hg_dict]
+    if isogg not in isogg_hg_to_snp_dict:
+        st.error(f"ISOGG haplogroup {isogg} not found")
+    if yfull not in yfull_hg_to_snp_dict:
+        st.error(f"YFull haplogroup {yfull} not found")
 
-if len(isogg_missing_snps) > 0:
-    st.error(f"These ISOGG SNPs are not present in YFull: {', '.join(isogg_missing_snps)}")
-if len(yfull_missing_snps) > 0:
-    st.error(f"These YFull SNPs are not present in ISOGG: {', '.join(yfull_missing_snps)}")
+    if isogg not in isogg_hg_to_snp_dict or yfull not in yfull_hg_to_snp_dict:
+        st.stop()
 
-isogg_present_snps = [i for i in isogg_identifying_snps if i in yfull_snp_to_hg_dict]
-yfull_present_snps = [i for i in yfull_identifying_snps if i in isogg_snp_to_hg_dict]
+    else:
+        isogg_path = get_path_to_root(isogg_tree, isogg)
+        yfull_path = get_path_to_root(yfull_tree, yfull)
 
-yfull_hg_isogg_snps = Counter([i for snp in isogg_present_snps for i in yfull_snp_to_hg_dict[snp]])
-isogg_hg_yfull_snps = Counter([i for snp in yfull_present_snps for i in isogg_snp_to_hg_dict[snp]])
+        isogg_identifying_snps = [i for i in isogg_hg_to_snp_dict.get(isogg)]  # if i in yfull_snp_to_hg_dict
+        yfull_identifying_snps = [i for i in yfull_hg_to_snp_dict.get(yfull)]  # if i in isogg_snp_to_hg_dict
 
-st.header("ISOGG resolution")
-st.write(f"**There are {len(isogg_hg_yfull_snps)} ISOGG haplogroups that correspond to these YFull SNPs:**")
-for hg in isogg_hg_yfull_snps:
-    st.write(f"Comparing Yfull based ISOGG haplogroup {hg} ({int(isogg_hg_yfull_snps[hg]/sum(isogg_hg_yfull_snps.values())*100)}%) to real ISOGG haplogroup {isogg}:")
-    path = get_path_to_root(isogg_tree, hg)
-    if path == isogg_path:
-        st.warning("Resolutions are the same")
-    elif len(path) < len(isogg_path):
-        if path == isogg_path[:len(path)]:
-            st.success(f"ISOGG resolution is {len(isogg_path) - len(path)} levels higher")
-        else:
-            lowest_common_ancestor = nx.lowest_common_ancestor(isogg_tree, path[-1], isogg_path[-1])
-            st.error(f"Paths do not match. Lowest common ancestor is {lowest_common_ancestor}.")
-    elif len(path) > len(isogg_path):
-        if isogg_path == path[:len(isogg_path)]:
-            st.success(f"YFull resolution is {len(path) - len(isogg_path)} levels higher")
-        else:
-            lowest_common_ancestor = nx.lowest_common_ancestor(isogg_tree, path[-1], isogg_path[-1])
-            st.error(f"Paths do not match. Lowest common ancestor is {lowest_common_ancestor}.")
+        isogg_missing_snps = [i for i in isogg_identifying_snps if i not in yfull_snp_to_hg_dict]
+        yfull_missing_snps = [i for i in yfull_identifying_snps if i not in isogg_snp_to_hg_dict]
 
-st.header("YFull resolution")
-st.write(f"**There are {len(yfull_hg_isogg_snps)} YFull haplogroups that correspond to these ISOGG SNPs:**")
-for hg in yfull_hg_isogg_snps:
-    st.write(f"Comparing ISOGG based YFull haplogroup {hg} ({int(yfull_hg_isogg_snps[hg]/sum(yfull_hg_isogg_snps.values())*100)}%) to real YFull haplogroup {yfull}:")
-    path = get_path_to_root(yfull_tree, hg)
-    if path == yfull_path:
-        st.warning("Resolutions are the same")
-    elif len(path) < len(yfull_path):
-        if path == yfull_path[:len(path)]:
-            st.success(f"YFull resolution is {len(yfull_path) - len(path)} levels higher")
-        else:
-            lowest_common_ancestor = nx.lowest_common_ancestor(yfull_tree, path[-1], yfull_path[-1])
-            st.error(f"Paths do not match. Lowest common ancestor is {lowest_common_ancestor}.")
-    elif len(path) > len(yfull_path):
-        if yfull_path == path[:len(yfull_path)]:
-            st.success(f"ISOGG resolution is {len(path) - len(yfull_path)} levels higher")
-        else:
-            lowest_common_ancestor = nx.lowest_common_ancestor(yfull_tree, path[-1], yfull_path[-1])
-            st.error(f"Paths do not match. Lowest common ancestor is {lowest_common_ancestor}.")
+        if len(isogg_missing_snps) > 0:
+            st.error(f"These ISOGG SNPs are not present in YFull: {', '.join(isogg_missing_snps)}")
+        if len(yfull_missing_snps) > 0:
+            st.error(f"These YFull SNPs are not present in ISOGG: {', '.join(yfull_missing_snps)}")
 
-st.header("Paths")
-st.write(f"Yfull path is:")
-st.info(' - '.join(yfull_path))
-st.write(f"ISOGG path is:")
-st.info(' - '.join(isogg_path))
+        isogg_present_snps = [i for i in isogg_identifying_snps if i in yfull_snp_to_hg_dict]
+        yfull_present_snps = [i for i in yfull_identifying_snps if i in isogg_snp_to_hg_dict]
 
-st.header("Identifying SNPs")
-st.write(f"Identifying SNPs for Yfull {yfull} are:")
-st.info(', '.join(yfull_identifying_snps))
-st.write(f"Identifying SNPs for ISOGG {isogg} are:")
-st.info(', '.join(isogg_identifying_snps))
+        yfull_hg_isogg_snps = Counter([i for snp in isogg_present_snps for i in yfull_snp_to_hg_dict[snp]])
+        isogg_hg_yfull_snps = Counter([i for snp in yfull_present_snps for i in isogg_snp_to_hg_dict[snp]])
 
-st.header("Haplogroups")
-st.write(f"ISOGG haplogroups for these Yfull SNPs are: {isogg_hg_yfull_snps}")
-st.write(f"YFull haplogroups for these ISOGG SNPs are: {yfull_hg_isogg_snps}")
+        st.header("ISOGG resolution")
+        st.write(f"**There are {len(isogg_hg_yfull_snps)} ISOGG haplogroups that correspond to these YFull SNPs:**")
+        for hg in isogg_hg_yfull_snps:
+            st.write(f"Comparing Yfull based ISOGG haplogroup {hg} ({int(isogg_hg_yfull_snps[hg]/sum(isogg_hg_yfull_snps.values())*100)}%) to real ISOGG haplogroup {isogg}:")
+            path = get_path_to_root(isogg_tree, hg)
+            if path == isogg_path:
+                st.warning("Resolutions are the same")
+            elif len(path) < len(isogg_path):
+                if path == isogg_path[:len(path)]:
+                    st.success(f"ISOGG resolution is {len(isogg_path) - len(path)} levels higher")
+                else:
+                    lowest_common_ancestor = nx.lowest_common_ancestor(isogg_tree, path[-1], isogg_path[-1])
+                    st.error(f"Paths do not match. Lowest common ancestor is {lowest_common_ancestor}.")
+            elif len(path) > len(isogg_path):
+                if isogg_path == path[:len(isogg_path)]:
+                    st.success(f"YFull resolution is {len(path) - len(isogg_path)} levels higher")
+                else:
+                    lowest_common_ancestor = nx.lowest_common_ancestor(isogg_tree, path[-1], isogg_path[-1])
+                    st.error(f"Paths do not match. Lowest common ancestor is {lowest_common_ancestor}.")
 
-st.header("Info")
-st.info("The resolution of the real ISOGG haplogroup is compared to the resolution of the YFull haplogroup based on ISOGGs identifying SNPs. "
-        "The percentage of each haplogroup is based on the number of identifying SNPs that support this haplogroup. "
-        "Asterisks (*) are currently ignored for comparison.")
+        st.header("YFull resolution")
+        st.write(f"**There are {len(yfull_hg_isogg_snps)} YFull haplogroups that correspond to these ISOGG SNPs:**")
+        for hg in yfull_hg_isogg_snps:
+            st.write(f"Comparing ISOGG based YFull haplogroup {hg} ({int(yfull_hg_isogg_snps[hg]/sum(yfull_hg_isogg_snps.values())*100)}%) to real YFull haplogroup {yfull}:")
+            path = get_path_to_root(yfull_tree, hg)
+            if path == yfull_path:
+                st.warning("Resolutions are the same")
+            elif len(path) < len(yfull_path):
+                if path == yfull_path[:len(path)]:
+                    st.success(f"YFull resolution is {len(yfull_path) - len(path)} levels higher")
+                else:
+                    lowest_common_ancestor = nx.lowest_common_ancestor(yfull_tree, path[-1], yfull_path[-1])
+                    st.error(f"Paths do not match. Lowest common ancestor is {lowest_common_ancestor}.")
+            elif len(path) > len(yfull_path):
+                if yfull_path == path[:len(yfull_path)]:
+                    st.success(f"ISOGG resolution is {len(path) - len(yfull_path)} levels higher")
+                else:
+                    lowest_common_ancestor = nx.lowest_common_ancestor(yfull_tree, path[-1], yfull_path[-1])
+                    st.error(f"Paths do not match. Lowest common ancestor is {lowest_common_ancestor}.")
+
+        st.header("Paths")
+        st.write(f"Yfull path is:")
+        st.info(' - '.join(yfull_path))
+        st.write(f"ISOGG path is:")
+        st.info(' - '.join(isogg_path))
+
+        st.header("Identifying SNPs")
+        st.write(f"Identifying SNPs for Yfull {yfull} are:")
+        st.info(', '.join(yfull_identifying_snps))
+        st.write(f"Identifying SNPs for ISOGG {isogg} are:")
+        st.info(', '.join(isogg_identifying_snps))
+
+        st.header("Haplogroups")
+        st.write(f"ISOGG haplogroups for these Yfull SNPs are: {isogg_hg_yfull_snps}")
+        st.write(f"YFull haplogroups for these ISOGG SNPs are: {yfull_hg_isogg_snps}")
+
+        st.header("Info")
+        st.info("The resolution of the real ISOGG haplogroup is compared to the resolution of the YFull haplogroup based on ISOGGs identifying SNPs. "
+                "The percentage of each haplogroup is based on the number of identifying SNPs that support this haplogroup. "
+                "Asterisks (*) are currently ignored for comparison.")
